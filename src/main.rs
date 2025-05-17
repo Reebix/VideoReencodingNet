c48c6136c6f81dca79e03df6dcba2dbdfb606308
My name <myemail@myorg.com>
2025-05-17 14:37:54 +0200

    add clap for command-line argument parsing and implement video length calculation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate rocket;
 
+use clap::Parser;
 use lazy_static::lazy_static;
 use rocket::data::{Limits, ToByteUnit};
 use rocket::fs::{NamedFile, TempFile};
@@ -20,11 +21,19 @@ struct Task<'r> {
     complete: bool,
 }
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Arguments {
+    #[arg(short, long, default_value_t = String::from(""))]
+    path: String,
+}
+
 lazy_static! {
     static ref FILE_LIST: Mutex<Vec<PathBuf>> = Mutex::new(Vec::new());
     static ref FILE_QUEUE: Mutex<VecDeque<PathBuf>> = Mutex::new(VecDeque::new());
     static ref SCANNED_FILES: Mutex<usize> = Mutex::new(0);
     static ref BASE_PATH: Mutex<String> = Mutex::new("".parse().unwrap());
+    static ref VIDEO_LENGTH: Mutex<f64> = Mutex::new(0f64);
 }
 
 #[get("/request")]
@@ -79,10 +88,12 @@ fn base() -> String {
         "Status:\n\
     Total Files: {}\n\
     Scanned Files: {}\n\
-    Files To Convert: {}",
+    Files To Convert: {}\n\
+    Total Length: {:.2}min",
         FILE_LIST.lock().unwrap().len(),
         SCANNED_FILES.lock().unwrap(),
-        FILE_QUEUE.lock().unwrap().len()
+        FILE_QUEUE.lock().unwrap().len(),
+        VIDEO_LENGTH.lock().unwrap()
     )
 }
 
@@ -140,6 +151,22 @@ fn get_codec_info(path: &Path) -> String {
     stdout.to_string()
 }
 
+fn get_video_length(path: &Path) -> String {
+    let output = Command::new("ffprobe")
+        .arg("-v")
+        .arg("error")
+        .arg("-show_entries")
+        .arg("format=duration")
+        .arg("-of")
+        .arg("default=noprint_wrappers=1:nokey=1")
+        .arg(path.to_str().unwrap())
+        .output()
+        .expect("Failed to execute command");
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    stdout.to_string()
+}
+
 fn clear_queue() {
     let mut fq = FILE_QUEUE.lock().unwrap();
     *fq = VecDeque::new();
@@ -151,6 +178,8 @@ fn clear_queue() {
 }
 
 async fn scan(path: &Path) {
+    let mut file_size = 0;
+    let mut length: f64 = 0f64;
     clear_queue();
     // get all files in the directory
     let files = get_all_files(path);
@@ -160,38 +189,66 @@ async fn scan(path: &Path) {
     for file in files {
         // get ffmpeg info
         let codec_info = get_codec_info(&file);
-        let codec = codec_info.lines().next().unwrap();
+        // let codec = codec_info.lines().next().unwrap();
         // check if the codec is h264
-        if codec == "h264" {
-            FILE_QUEUE.lock().unwrap().push_back(file.clone());
-        }
-        let mut scanned_files = SCANNED_FILES.lock().unwrap();
-        *scanned_files += 1;
+        // if codec == "h264" {
+        //     FILE_QUEUE.lock().unwrap().push_back(file.clone());
+        // }
+
+        file_size += file.metadata().unwrap().len();
+
+        tokio::spawn(async move {
+            add_file_length(&file).await;
+            let mut scanned_files = SCANNED_FILES.lock().unwrap();
+            *scanned_files += 1;
+        });
     }
     println!(
         "{} of Which where in the wrong codec",
         FILE_QUEUE.lock().unwrap().len()
     );
+
+    // format size of files
+    let file_size = file_size as f64 / 1_000_000.0;
+    let file_size = format!("{:.2}", file_size);
+    println!("Total size of files: {} MB", file_size);
 }
 
-#[launch]
+async fn add_file_length(path: &Path) {
+    let length_info = get_video_length(path);
+    let length_info = length_info.trim();
+    let length_info = length_info.parse::<f64>().unwrap();
+    let length_info = length_info / 60.0;
+    let length_info = format!("{:.2}", length_info);
+    *VIDEO_LENGTH.lock().unwrap() += length_info.parse::<f64>().unwrap();
+}
+
 #[tokio::main]
-async fn rocket() -> _ {
+async fn main() {
+    let args = Arguments::parse();
+
+    // print current user
     // get input from user
     let mut input = String::new();
-    #[cfg(not(debug_assertions))]
-    {
-        println!("Enter the path where the server should look for files: ");
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
+    if !args.path.is_empty() {
+        input = args.path;
+        println!("base_path: {}", input);
+    } else {
+        #[cfg(not(debug_assertions))]
+        {
+            println!("Enter the path where the server should look for files: ");
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read line");
+        }
+        #[cfg(debug_assertions)]
+        {
+            println!("Debug mode: using default file path");
+            input = "C:\\Users\\Rebix\\Downloads\\testcompressions".to_string()
+        }
+
+        input = input.trim().parse().unwrap();
     }
-    #[cfg(debug_assertions)]
-    {
-        println!("Debug mode: using default file path");
-        input = "C:\\Users\\Rebix\\Downloads\\testcompressions".to_string()
-    }
-    let input = input.trim();
     scan_files(input.to_string()).await;
 
     rocket::build()
@@ -206,4 +263,7 @@ async fn rocket() -> _ {
         .mount("/", routes![files])
         .mount("/", routes![converted])
         .mount("/", routes![scan_files])
+        .launch()
+        .await
+        .expect("TODO: panic message");
 }
